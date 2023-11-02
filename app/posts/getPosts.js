0163adf16c732aa570a059834a3ec1c90c0113c9
Shubham shah <shahshubham1888@gmail.com>
2023-11-02 17:28:40 +0545

    added revalidate but didn't worked

--- app/posts/getPosts.js
+++ app/posts/getPosts.js
@@ -2,6 +2,7 @@
 import connect from "@/app/lib/mongodb";
 import AddPost from "@/app/models/AddPost";
 
+
 const getPosts = async () => {
     await connect()
     let data = await AddPost.find()
@@ -9,3 +10,4 @@ const getPosts = async () => {
 }
 
 export default getPosts
+// export const revalidate = 0; // this is the new line added
